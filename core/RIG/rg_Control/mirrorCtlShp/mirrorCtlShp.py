ad4ec0c35a42ed31bb29117ea4e5114de56de56b
changlong.zang <zclongpop123@163.com>
2015-05-08 14:55:43 +0800

    fix mirror control bug...

--- core/RIG/rg_Control/mirrorCtlShp/mirrorCtlShp.py
+++ core/RIG/rg_Control/mirrorCtlShp/mirrorCtlShp.py
@@ -3,7 +3,7 @@
 #   mail: zclongpop@163.com
 #   date: Tue, 05 May 2015 11:44:00
 #========================================
-import os.path, re
+import os.path, re, pymel.core
 import maya.cmds as mc
 from mpUtils import scriptTool, uiTool, mayaTool
 #--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
@@ -19,7 +19,7 @@ class MirrorControlShp(windowClass, baseClass):
     def on_btn_mirror_clicked(self, click=None):
         if click == None:return
         controlType = str(self.fld_controlType.text())
-        
+
         flipAxis = 'X'
         if self.rdn_filpX.isChecked():
             flipAxis = 'X'
@@ -42,61 +42,63 @@ def mirrorControlShape(typ, source, targent, flipAxis):
     if len(typ) == 0:return
     if source not in 'LR':return
     if source == targent:return
-    
+
     #- get source side controls
     all_controls     = ' '.join(mc.listRelatives(mc.ls(type='nurbsCurve'), p=True, path=True))
     matched_controls = re.findall('\S*%s_\w+_%s_\d+'%(source, typ), all_controls)
-    
+
     for ctl in matched_controls:
         #- get targent control
         targentControl = re.sub('%s_'%source, '%s_'%targent, ctl)
         if not mc.objExists(targentControl):continue
-        
+
         #- duplicate shape
         tempx = mc.duplicate(ctl, po=True)
         mc.parent(mc.listRelatives(ctl, s=True, path=True), tempx, s=True, add=True)
-        
+
         #- make Temp
-        Temp = mc.duplicate(tempx)[0]
+        Temp = pymel.core.PyNode(mc.duplicate(tempx, rc=True)[0])
         for a in 'trs':
             for b in 'xyz':
                 attr = a + b
                 mc.setAttr('%s.%s'%(Temp, attr), l=False, k=True, cb=False)  
 
         #- close max min value controler
-        mc.transformLimits(Temp, etx=(0, 0),ety=(0, 0),etz=(0, 0),erx=(0, 0),ery=(0, 0),erz=(0, 0))
-        Temp = mc.parent(Temp, w=True)
-        
+        mc.transformLimits(Temp.name(), etx=(0, 0),ety=(0, 0),etz=(0, 0),erx=(0, 0),ery=(0, 0),erz=(0, 0))
+        mc.parent(Temp.name(), w=True)
+
         #- filp
         grp = mc.createNode('transform')
-        
+
         sourcePosi = mc.xform(ctl, q=True, ws=True, rp=True)
         targenPosi = mc.xform(targentControl, q=True, ws=True, rp=True)
-        
+
         midPoint = [(sourcePosi[0] + targenPosi[0]) / 2,
                     (sourcePosi[0] + targenPosi[0]) / 2,
                     (sourcePosi[0] + targenPosi[0]) / 2]
         mc.move(midPoint[0], midPoint[1], midPoint[2], grp, a=True)
 
 
-        Temp = mc.parent(Temp, grp)
+        mc.parent(Temp.name(), grp)
         mc.setAttr('%s.s%s'%(grp, flipAxis.lower()), -1)
-        
+
         #- freeze transformations
-        Temp = mc.parent(Temp, targentControl)
-        mc.makeIdentity(Temp, apply=True, t=True, r=True, s=True)
-        
+        mc.parent(Temp.name(), targentControl)
+        mc.makeIdentity(Temp.name(), apply=True, t=True, r=True, s=True)
+
         #- get original shapes
-        originalShapes = mc.listRelatives(targentControl, s=True, path=True)
-        
+        originalShapes = mc.listRelatives(targentControl, s=True, path=True, type='nurbsCurve')
+        mc.delete(originalShapes)
+
         #- parent new shapes
-        shapes = mc.listRelatives(Temp, s=True, path=True)
+        shapes = mc.listRelatives(Temp.name(), s=True, path=True, type='nurbsCurve')
         for shp in shapes:
             mc.setAttr('%s.ovc'%shp, mc.getAttr('%s.ovc'%originalShapes[0]))
         mc.parent(shapes, targentControl, s=True, r=True)
+        
         for shp in shapes:
             mc.rename(shp, '%sShape'%targentControl)
-                  
+
         #- delete temp
-        mc.delete(tempx, Temp, grp, originalShapes)
-        
+        mc.delete(tempx, Temp.name(), grp)
+
