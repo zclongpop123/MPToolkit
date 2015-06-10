3c949d27b36911e67c317480ff259dd921edbebc
changlong.zang <zclongpop123@163.com>
2015-06-10 09:54:25 +0800

    change moudule names...

--- core/RIG/__init__.py
+++ core/RIG/__init__.py
@@ -3,9 +3,9 @@
 #   mail: zclongpop@163.com
 #   date: Thu, 30 Apr 2015 09:47:27
 #========================================
-import rg_Joint, rg_Control, rg_BlendShape, rg_DrivenKey, rg_Weight, rg_Plug
+import Joint, Control, BlendShape, DrivenKey, Weight, Plug
 #--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-class Rig_Class(rg_Joint.Mod_Class, rg_Control.Mod_Class, rg_BlendShape.Mod_Class, rg_DrivenKey.Mod_Class, rg_Weight.Mod_Class, rg_Plug.Mod_Class):
+class Rig_Class(Joint.Mod_Class, Control.Mod_Class, BlendShape.Mod_Class, DrivenKey.Mod_Class, Weight.Mod_Class, Plug.Mod_Class):
     '''
     Rigging team classes...
     '''
